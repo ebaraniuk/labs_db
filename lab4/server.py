6e98e07f7f8582cc737aee3cfaaf0e385109a364
ebaraniuk <ebaraniuk02@gmail.com>
2021-06-07 23:02:02 +0300

    fixed issues

--- lab4/server.py
+++ lab4/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from main import Psql
 
 app = Flask(__name__)
-psql = Psql('021001')
+psql = Psql(input())
 
 
 @app.route("/", methods=['post', 'get'])
